6c058df3f4a9f2d7dfe01f65b3e38ff0dbbea9bb
Norberto <norberspinella@gmail.com>
2026-08-25 15:44:52 +0200

    style - bajar foto circular a top 190px e icono plan a top 322 en PetProfileCard

--- src/components/mural/MuralCanvas.tsx
+++ src/components/mural/MuralCanvas.tsx
@@ -392,7 +392,7 @@ function PetProfileCard({ memorialId, planType, thumbnailUrl, onClose }: PetProf
                   alt="Plan"
                   style={{
                     position: 'absolute',
-                    top: 307,
+                    top: 322,
                     left: 189,
                     width: 72,
                     height: 72,
@@ -405,7 +405,7 @@ function PetProfileCard({ memorialId, planType, thumbnailUrl, onClose }: PetProf
               <div 
                 style={{
                   position: 'absolute',
-                  top: '175px',
+                  top: '190px',
                   left: '50%',
                   transform: 'translateX(-50%)',
                   width: '180px',
